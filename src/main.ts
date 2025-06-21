f3312244c44948866b299ec203ae4edc223be638
zuamirgoliveira <zuamirgutemberg@gmail.com>
2025-06-20 23:32:16 -0300

    feat: Implementa rate limite na api

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import rateLimit from 'express-rate-limit';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  app.use(
+    rateLimit({
+      windowMs: 60_000,
+      max: 20,
+      standardHeaders: true,
+      legacyHeaders: false,
+      message: {
+        statusCode: 429,
+        error: 'Too Many Requests',
+        message: 'Você excedeu o limite de requisições. Tente novamente mais tarde.',
+      },
+    }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('PR Tracker BFF')
